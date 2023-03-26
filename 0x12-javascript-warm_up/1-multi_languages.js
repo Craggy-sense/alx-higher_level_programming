6e846694a8623dce4034d1ad8f7baad8da84ed65
Craggy-sense <memusirobi@gmail.com>
2023-03-25 21:33:03 -0700

    task1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-let FirstLine = "C is fun";
-let SecondLine = "Python is cool";
-let ThirdLine = "JavaScript is amazing";
 
-console.log(FirstLine, SecondLine, ThirdLine);
+console.log("C is fun");
+console.log("Python is cool");
+console.log( "JavaScript is amazing");
+
